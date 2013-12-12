050da77e84e1c3a27a172a18778ed72606fde034
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-12-13 01:58:27 +0900

    minor fix

--- lib/qiita.ex
+++ lib/qiita.ex
@@ -1,2 +1,12 @@
 defmodule Qiita do
+  use HTTPotion.Base
+  def process_url(url) do
+    "https://qiita.com/api/v1/" <> url
+  end
+
+  def process_response_body(body) do
+    json = :jsx.decode to_string(body)
+    atomize_keys_json = Enum.map json, fn ({k,v}) -> { binary_to_atom(k), v } end
+    :orddict.from_list atomize_keys_json
+  end
 end
